d836007e68fe668fe6a9e8a1ba727eb9a4538195
Adam Goldsmith <adam@adamgoldsmith.name>
2023-10-21 15:39:35 -0400

    Add Magisk module to set SELinux permissions for aguiExtModule

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
+
+tasks.register<Zip>("magisk_module") {
+    from(file("magisk_module"))
+    archiveFileName = "open_aguiExtModule_permissions.zip"
+    destinationDirectory = file(layout.buildDirectory)
+}
